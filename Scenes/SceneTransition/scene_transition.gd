ef6b6b9af940943f78785eb734c5e79fb6e52961
TakaVII <ulirub@gmail.com>
2025-08-08 17:50:53 -0400

    Added scene transition logic + scenes (bugged scene transition)

--- Scenes/SceneTransition/scene_transition.gd
+++ Scenes/SceneTransition/scene_transition.gd
@@ -1,15 +1,13 @@
 extends CanvasLayer
 
-var animation_player : AnimationPlayer
+@onready var animation_player : AnimationPlayer = $Control/AnimationPlayer
 
 func fade_out() -> bool:
-	animation_player = $Control/AnimationPlayer
 	animation_player.play("fade_out")
 	await animation_player.animation_finished
 	return true
 	
 func fade_in() -> bool:
-	animation_player = $Control/AnimationPlayer
 	animation_player.play("fade_in")
 	await animation_player.animation_finished
 	return true	
